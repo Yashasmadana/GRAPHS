f1fdbe8f40167a8d16130c717046bbb612432071
Yashasmadana <85828528+Yashasmadana@users.noreply.github.com>
2023-11-14 19:28:34 +0530

    Update graph_implementation_using_graph_library.cpp

--- graph_implementation_using_graph_library.cpp
+++ graph_implementation_using_graph_library.cpp
@@ -3,6 +3,8 @@
 int main()
 {
     Graph g;
+
+    // directed graph
     g.dir_addEdge(0, 4);
     g.dir_addEdge(0, 3);
     g.dir_addEdge(1, 2);
@@ -10,7 +12,14 @@ int main()
     g.dir_addEdge(4, 3);
     g.dir_addEdge(3, 5);
     g.dir_addEdge(0, 6);
-    // Function call
+    // Undirected graph
+    g.addEdge(0, 4);
+    g.addEdge(0, 3);
+    g.addEdge(1, 2);
+    g.addEdge(4, 1);
+    g.addEdge(4, 3);
+    g.addEdge(3, 5);
+    g.addEdge(0, 6);
     g.BFS(4);
 
     return 0;
