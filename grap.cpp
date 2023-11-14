dcd83dc2af5807eb2bfd5c0b88355fc36499cf21
Yashasmadana <85828528+Yashasmadana@users.noreply.github.com>
2023-11-14 19:26:26 +0530

    Add files via upload

---
+++ grap.cpp
@@ -0,0 +1,17 @@
+#include "GRAPH.h"
+
+int main()
+{
+    Graph g;
+    g.dir_addEdge(0, 4);
+    g.dir_addEdge(0, 3);
+    g.dir_addEdge(1, 2);
+    g.dir_addEdge(4, 1);
+    g.dir_addEdge(4, 3);
+    g.dir_addEdge(3, 5);
+    g.dir_addEdge(0, 6);
+    // Function call
+    g.BFS(4);
+
+    return 0;
+}
